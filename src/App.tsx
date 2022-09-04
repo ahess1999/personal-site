128b1561b993572c72227e6996c4778b5b060246
Austin Hess <ahess@ksu.edu>
2022-09-04 14:45:24 -0500

    added comment

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Homepage from "./SiteHomepage/Pages/Homepage";
-
+//Site v1
 function App() {
   let location = useLocation();
   return (
